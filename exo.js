f2276a4902722e4428361ef1af00f5be0991e07e
Dovahgolz <114467805+Dovahgolz@users.noreply.github.com>
2023-03-09 08:38:20 +0100

    clone

---
+++ exo.js
@@ -0,0 +1,9 @@
+let elt = document.getElementById('main');
+elt.innerHTML = "<div>Je suis la div 1</div><div>Je suis la div 2</div><div>Je suis la div 3</div>";
+
+var newElm = document.createElement("div");
+newElm.innerHTML = "<div>Je suis la div 4</div><div>Je suis la div 5</div><div>Je suis la div 6</div>";
+elt.appendChild(newElm);
+
+newElm.innerHTML = "<span>Je suis le span 1</span> <span>Je suis le span 2</span>";
+elt.replaceChild(document.createElement ("section"), newElt);
